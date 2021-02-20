8131ff9b92522daffcae67c1818cc06fe46087ab
Owen Tsai <owentsai.v@gmail.com>
2021-02-20 17:18:40 +0800

    sync: weekly update

---
+++ src/packages/button/button.vue
@@ -0,0 +1,16 @@
+<template>
+  <button
+    :class="[
+      'sui-button',
+      `sui-button--${type}`,
+      size ? `sui-button--${size}` : ''
+    ]"
+  ></button>
+</template>
+
+<script>
+  export default {
+    name: 'SButton',
+    
+  }
+</script>
