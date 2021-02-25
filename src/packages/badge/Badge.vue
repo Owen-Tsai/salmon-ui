4eb6ac427d614ad891e06caab2bc554415272398
Owen Tsai <owentsai.v@gmail.com>
2021-02-25 17:40:05 +0800

    sync

---
+++ src/packages/badge/Badge.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="sui-badge" :class="`sui-badge--${type}`">
+    <slot></slot>
+
+    <transition name="zoom-in-center">
+      <sup
+        v-show="!hidden && (content || isDot)"
+        class="sui-badge__content"
+        :class=""
+      ></sup>
+    </transition>
+  </div>
+</template>
+
+<script>
+  import { defineComponent, ref, computed } from 'vue'
+
+  const _types = ['default', 'success', 'warning', 'danger']
+
+  export default defineComponent({
+    name: 'SBadge',
+    props: {
+      type: {
+        type: String,
+        default: 'default',
+
+      }
+    }
+  })
+</script>
