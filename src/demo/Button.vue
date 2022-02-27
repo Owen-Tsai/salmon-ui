bafed4c86d4d5e5493476550a5be0a92f9c44afa
Owen Tsai <owentsai.v@gmail.com>
2022-02-27 19:16:04 +0800

    chore: refactor structure of examples

---
+++ src/demo/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <demo name="Button">
+    <s-button type="primary">主要按钮</s-button>
+  </demo>
+</template>
+
+<script lang="ts">
+import SButton from 'salmon-ui/button'
+import Demo from '@/demo/components/Demo.vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  components: {
+    SButton,
+    Demo
+  }
+})
+</script>
+
+<style lang="stylus" scoped></style>
---
+++ src/demo/components/Demo.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="demo-container">
+    <div class="demo-container-header">
+      <h2>{{ name }}</h2>
+    </div>
+
+    <div class="demo-container-body">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  PropType
+} from 'vue'
+
+export default defineComponent({
+  name: 'Demo',
+  props: {
+    name: String as PropType<string>
+  }
+})
+</script>
+
+<style scoped>
+.demo-container {
+  border: 1px solid #d1d5db;
+}
+.demo-container-header {
+  display: flex;
+  align-items: center;
+  padding: 8px 16px;
+  background-color: #f9fafb;
+}
+.demo-container-body {
+  padding: 16px;
+}
+</style>
