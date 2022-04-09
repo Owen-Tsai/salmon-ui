0399d2ce18053ff95a56cc49b3bc2caf43e0bc1c
Owen Tsai <owentsai.v@gmail.com>
2022-04-09 19:00:54 +0800

    refactor(radio): extract mixin to reset default input element

--- src/demo/Radio.vue
+++ src/demo/Radio.vue
@@ -15,14 +15,23 @@
     <p>Disabled</p>
     <s-radio disabled v-model="model3" name="group-disabled" value="1">Disabled</s-radio>
     <s-radio disabled v-model="model3" name="group-disabled" value="2">Disabled</s-radio>
+    <p>Grouped: {{ model4 }}</p>
+    <s-radio-group v-model="model4">
+      <s-radio :value="1">Jinan</s-radio>
+      <s-radio :value="2">Beijing</s-radio>
+      <s-radio :value="3">Chengdu</s-radio>
+      <s-radio :value="4">London</s-radio>
+    </s-radio-group>
   </demo>
 </template>
 
 <script lang="ts" setup>
 import SRadio from 'salmon-ui/radio'
+import SRadioGroup from 'salmon-ui/radio-group'
 import { ref } from 'vue'
 
 const model = ref(false)
 const model2 = ref(1)
 const model3 = ref('1')
+const model4 = ref(1)
 </script>
