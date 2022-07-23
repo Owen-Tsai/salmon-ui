d1bf4e471b7c3ee9a881a36b13d76a95d0fae969
Owen Tsai <owentsai.v@gmail.com>
2022-07-23 11:15:02 +0800

    sync

--- src/demo/Avatar.vue
+++ src/demo/Avatar.vue
@@ -1,25 +1,24 @@
 <template>
   <demo name="Avatar">
     <s-avatar :src="img" />
-    <s-avatar :src="img2" />
+    <s-avatar :src="img3" />
     <s-avatar>User</s-avatar>
     <s-avatar>
       <s-icon :size="24"><user-fill /></s-icon>
     </s-avatar>
     <br>
-    <s-avatar shape="circle" :src="img" />
     <s-avatar shape="circle" :src="img3" />
     <s-avatar shape="circle" auto-font-size>张三</s-avatar>
     <s-avatar shape="circle" auto-font-size>{{ text }}</s-avatar>
     <s-avatar shape="circle" auto-font-size>M</s-avatar>
     <br>
     <s-avatar size="large" :src="img" />
-    <s-avatar size="large" shape="circle" :src="img2" />
+    <s-avatar size="large" shape="circle" :src="img3" />
     <s-avatar size="small" shape="circle" :src="img" />
-    <s-avatar size="small" :src="img2" />
+    <s-avatar size="small" :src="img3" />
     <br>
     <s-avatar :size="56" shape="circle" :src="img" />
-    <s-avatar :size="56" :src="img2" />
+    <s-avatar :size="56" :src="img3" />
   </demo>
 </template>
 
@@ -27,7 +26,6 @@
 import { ref } from 'vue'
 import SAvatar from 'salmon-ui/avatar'
 import img from '@/assets/avatar.jpg'
-import img2 from '@/assets/avatar3.jpg'
 import img3 from '@/assets/avatar2.jpg'
 import { UserFill } from '@salmon-ui/icons'
 
